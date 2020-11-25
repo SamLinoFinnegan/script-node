6d42d356e7dae106d56021922d9010d163315383
SamLinoFinnegan <73761381+SamLinoFinnegan@users.noreply.github.com>
2020-11-25 08:39:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let resultMult = 1230941 * 1823794;
+let resultDiv = 637263 / 54;
+
+console.log("Hello Winc Academy students")
+
+//This is a large sum
+console.log(resultMult)
+
+console.log(resultDiv)
